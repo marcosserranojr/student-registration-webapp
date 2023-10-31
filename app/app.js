6942d1a88f926443e16869eb59d440e16510b876
Serrano <Marcos.SerranoJr@va.gov>
2023-10-31 08:13:19 -0500

    intial commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-
 const port=3000
-const VERSION=1.0
 
+app.get((req, res) => {
+  res.send('Application Running')
+});  
  
 app.listen(port, () => {
-  console.log(`Application version ${VERSION} is listening on port ${port}...`)
+  console.log(`Application is listening on port ${port}...`)
 });
 app.use(express.static(__dirname +'/public')); //where your static content is located in your filesystem
 
